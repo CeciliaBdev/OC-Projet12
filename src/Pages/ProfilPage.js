bf89c27f2a94cd710f2819fc7b2b3558ee776806
CeciliaBdev <cecilia.bernard@live.fr>
2022-10-25 09:46:30 +0200

    update firstname profile

--- src/Pages/ProfilPage.js
+++ src/Pages/ProfilPage.js
@@ -1,7 +1,42 @@
-function Profil() {
-  console.log('test')
+import NavBar from '../components/NavBar.js'
+import UserInfos from '../components/UserInfos.js'
+import '../styles/Dashboard.css'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { allData } from '../services/allData.js'
+import { getUserMainData } from '../services/callApi.js'
 
-  return <div>Profil</div>
+function Profil() {
+  //hooks
+  const { id } = useParams()
+  const [userData, setUserData] = useState()
+
+  useEffect(() => {
+    getUserMainData(id)
+      .then((values) => {
+        console.log('id', id)
+        console.log('values', values['data']['userInfos']['firstName'])
+        setUserData(values['data']['userInfos']['firstName']) //firstname
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [id])
+
+  if (userData === undefined) {
+    console.log('error data')
+  }
+
+  return (
+    <div>
+      <div>
+        <NavBar />
+        <div className="dashboard">
+          <UserInfos firstName={userData} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Profil
---
+++ src/components/UserInfos.js
@@ -0,0 +1,19 @@
+// attention a l'écriture de propTypes
+// utilisation des propTypes : validation de types des props d'un composant
+
+import propTypes from 'prop-types'
+
+function UserInfos({ firstName }) {
+  return (
+    <div>
+      <h1>
+        Bonjour <span style={{ color: 'red' }}>{firstName}</span>
+      </h1>
+      <p>Félicitation ! Vous avez explosé vos objectifs hier 👏 </p>
+    </div>
+  )
+}
+UserInfos.propTypes = {
+  firstName: propTypes.string,
+}
+export default UserInfos
