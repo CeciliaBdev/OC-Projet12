9a43a1921532586a5c3b9fe70a7e81b9c76032f6
CeciliaBdev <cecilia.bernard@live.fr>
2022-10-25 09:44:52 +0200

    update component navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,9 @@
 import logo from '../assets/logo.svg'
+import icon_muscu from '../assets/icon_muscu.svg'
+import icon_bike from '../assets/icon_bike.svg'
+import icon_swim from '../assets/icon_swim.svg'
+import icon_yoga from '../assets/icon_yoga.svg'
+
 import '../styles/NavBar.css'
 
 function NavBar() {
@@ -11,7 +16,14 @@ function NavBar() {
         <a href="">Réglage</a>
         <a href="">Communauté</a>
       </div>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <div className="icons_sport">
+          <img src={icon_yoga} alt="" />
+          <img src={icon_swim} alt="" />
+          <img src={icon_bike} alt="" />
+          <img src={icon_muscu} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
