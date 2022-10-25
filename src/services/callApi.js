3132f1375baa489980fd8100d24e9017fa15c33e
CeciliaBdev <cecilia.bernard@live.fr>
2022-10-25 09:46:58 +0200

    call api

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -1,3 +1,15 @@
+import axios from 'axios'
 
+// functions
+// @function getUserMainData
 
-
+export const getUserMainData = async (id) => {
+  try {
+    const response = await axios.get(`http://localhost:3000/user/${id}`)
+    console.log('id api', id)
+    console.log('id response', response)
+    return response.data
+  } catch (error) {
+    console.log('error api', error)
+  }
+}
