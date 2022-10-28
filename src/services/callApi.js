8a8c0ec955aaa6d43a8e26e6d7f4b235e710ef28
CeciliaBdev <cecilia.bernard@live.fr>
2022-10-28 14:34:02 +0200

    update api performance

--- src/services/callApi.js
+++ src/services/callApi.js
@@ -2,14 +2,49 @@ import axios from 'axios'
 
 // functions
 // @function getUserMainData
+// @function getUserActivity
+// @function getUserAverageSession
 
 export const getUserMainData = async (id) => {
   try {
     const response = await axios.get(`http://localhost:3000/user/${id}`)
-    console.log('id api', id)
-    console.log('id response', response)
+    // console.log('id api', id)
+    // console.log('id response', response)
     return response.data
   } catch (error) {
-    console.log('error api', error)
+    console.log('error', error)
+  }
+}
+// poids - calories
+export const getUserActivity = async (id) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/user/${id}/activity`
+    )
+    return response.data
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+// moyenne sessions
+export const getUserAverageSession = async (id) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/user/${id}/average-sessions`
+    )
+    return response.data
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+//performance
+export const getUserPerformance = async (id) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/user/${id}/performance`
+    )
+    return response.data
+  } catch (error) {
+    console.log('error', error)
   }
 }
