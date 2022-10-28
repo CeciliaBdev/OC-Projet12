35699645cabf495b4dc3da8d47f591fd8556c92d
CeciliaBdev <cecilia.bernard@live.fr>
2022-10-28 14:33:16 +0200

    update component radarchart

--- src/Pages/ProfilPage.js
+++ src/Pages/ProfilPage.js
@@ -2,11 +2,17 @@ import NavBar from '../components/NavBar.js'
 import UserInfos from '../components/UserInfos.js'
 import FoodInfos from '../components/FoodInfos.js'
 import Bargraph from '../components/BarChart.js'
-import Graph from '../components/Graph.js'
+
 import '../styles/Dashboard.css'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { getUserMainData, getUserActivity } from '../services/callApi.js'
+import {
+  getUserMainData,
+  getUserActivity,
+  getUserPerformance,
+} from '../services/callApi.js'
+
+import Radargraph from '../components/RadarChart.js'
 
 function Profil() {
   //hooks
@@ -14,6 +20,8 @@ function Profil() {
   const [userData, setUserData] = useState() // firstname
   const [keyData, setKeyData] = useState() // keyData
   const [activity, setActivity] = useState() //activité
+  // const [averageSessions, setAverageSessions] = useState([]) //moyenne session
+  const [performance, setPerformance] = useState([])
 
   useEffect(() => {
     getUserMainData(id).then((values) => {
@@ -23,12 +31,20 @@ function Profil() {
       // console.log(values['data']['keyData'])
       setKeyData(values['data']['keyData']) // keyData
     })
-    getUserActivity(id)
+    getUserActivity(id).then((values) => {
+      // console.log('values', values['data']['sessions'])
+      setActivity(values['data']['sessions'])
+    })
+    // getUserAverageSession(id)
+    //   .then((values) => {
+    //     console.log('values sessions', values['data']['sessions'])
+    //     setAverageSessions('values', values['data']['sessions'])
+    //   })
+    getUserPerformance(id)
       .then((values) => {
-        console.log('values', values['data']['sessions'])
-        setActivity(values['data']['sessions'])
+        console.log(values['data']['data'])
+        setPerformance(values['data']['data'])
       })
-
       .catch((error) => {
         console.log(error)
       })
@@ -37,6 +53,120 @@ function Profil() {
   // if (userData === undefined) {
   //   console.log('error data')
   // }
+  // const data = [
+  //   {
+  //     subject: 'Math',
+  //     A: 120,
+  //     B: 110,
+  //     fullMark: 150,
+  //   },
+  //   {
+  //     subject: 'Chinese',
+  //     A: 98,
+  //     B: 130,
+  //     fullMark: 150,
+  //   },
+  //   {
+  //     subject: 'English',
+  //     A: 86,
+  //     B: 130,
+  //     fullMark: 150,
+  //   },
+  //   {
+  //     subject: 'Geography',
+  //     A: 99,
+  //     B: 100,
+  //     fullMark: 150,
+  //   },
+  //   {
+  //     subject: 'Physics',
+  //     A: 85,
+  //     B: 90,
+  //     fullMark: 150,
+  //   },
+  //   {
+  //     subject: 'History',
+  //     A: 65,
+  //     B: 85,
+  //     fullMark: 150,
+  //   },
+  // ]
+  // const USER_PERFORMANCE = [
+  //   {
+  //     userId: 12,
+  //     kind: {
+  //       1: 'cardio',
+  //       2: 'energy',
+  //       3: 'endurance',
+  //       4: 'strength',
+  //       5: 'speed',
+  //       6: 'intensity',
+  //     },
+  //     data: [
+  //       {
+  //         value: 80,
+  //         kind: 1,
+  //       },
+  //       {
+  //         value: 120,
+  //         kind: 2,
+  //       },
+  //       {
+  //         value: 140,
+  //         kind: 3,
+  //       },
+  //       {
+  //         value: 50,
+  //         kind: 4,
+  //       },
+  //       {
+  //         value: 200,
+  //         kind: 5,
+  //       },
+  //       {
+  //         value: 90,
+  //         kind: 6,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     userId: 18,
+  //     kind: {
+  //       1: 'cardio',
+  //       2: 'energy',
+  //       3: 'endurance',
+  //       4: 'strength',
+  //       5: 'speed',
+  //       6: 'intensity',
+  //     },
+  //     data: [
+  //       {
+  //         value: 200,
+  //         kind: 1,
+  //       },
+  //       {
+  //         value: 240,
+  //         kind: 2,
+  //       },
+  //       {
+  //         value: 80,
+  //         kind: 3,
+  //       },
+  //       {
+  //         value: 80,
+  //         kind: 4,
+  //       },
+  //       {
+  //         value: 220,
+  //         kind: 5,
+  //       },
+  //       {
+  //         value: 110,
+  //         kind: 6,
+  //       },
+  //     ],
+  //   },
+  // ]
 
   return (
     <div>
@@ -47,7 +177,14 @@ function Profil() {
           <div className="dashboard">
             <div className="graph_container">
               <Bargraph activity={activity} />
-              <Graph />
+              <div className="graphs">
+                <div></div>
+                <div>
+                  <Radargraph performance={performance} />
+                </div>
+
+                <div></div>
+              </div>
             </div>
             <FoodInfos keyData={keyData} />
           </div>
---
+++ src/components/RadarChart.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import {
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis,
+  Radar,
+  ResponsiveContainer,
+} from 'recharts'
+import PropTypes from 'prop-types'
+
+function namePerf(kind) {
+  switch (kind) {
+    case 1:
+      return 'Intensité'
+    case 2:
+      return 'Vitesse'
+    case 3:
+      return 'Force'
+    case 4:
+      return 'Endurance'
+    case 5:
+      return 'Energie'
+    case 6:
+      return 'Cardio'
+    default:
+      return null
+  }
+}
+function Radargraph({ performance }) {
+  return (
+    <ResponsiveContainer>
+      <RadarChart
+        margin={{ top: 20, right: 20, bottom: 20, left: 70 }}
+        style={{
+          backgroundColor: '#282D30',
+          borderRadius: '5px',
+        }}
+        data={performance}
+        width={220}
+        height={224}
+        outerRadius={80}
+      >
+        <PolarGrid radialLines={false} />
+
+        <PolarAngleAxis
+          dataKey="kind"
+          tickFormatter={namePerf}
+          tickLine={false}
+          axisLine={false}
+          dy={5}
+          stroke="#FFF"
+          tick={{ fill: '#FFFFFF', fontSize: 13 }}
+        />
+
+        <PolarRadiusAxis tick={false} tickCount={6} axisLine={false} />
+
+        <Radar dataKey="value" fill="#FF0101B2" fillOpacity={0.9} />
+      </RadarChart>
+    </ResponsiveContainer>
+  )
+}
+
+Radargraph.propTypes = {
+  performance: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      kind: PropTypes.number.isRequired,
+    })
+  ),
+}
+export default Radargraph
