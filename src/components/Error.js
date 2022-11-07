54a15a41345b12f25a846daddd699ed712190d76
CeciliaBdev <cecilia.bernard@live.fr>
2022-11-07 16:04:41 +0100

    update error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,4 +1,17 @@
+import NavBar from '../components/NavBar.js'
+import '../styles/Error.css'
+
 function Error() {
-  return <div>Erreur api</div>
+  return (
+    <div>
+      <NavBar />
+      <div className="error_container ">
+        <div className="content">
+          <h1>Erreur 404</h1>
+          <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Error
